9ae3f1399fca9e841f118eaacbc258632cc68191
madhav vengalil <madhav1709@gmail.com>
2019-08-24 12:19:42 -0500

    Added UseDefaultFiles and UseStaticFiles Middleware

--- employeeManagement/Startup.cs
+++ employeeManagement/Startup.cs
@@ -5,12 +5,19 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace employeeManagement
 {
 	public class Startup
 	{
+		private IConfiguration _config;
+
+		public Startup(IConfiguration config)
+		{
+			_config = config;
+		}
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
@@ -24,10 +31,12 @@ namespace employeeManagement
 			{
 				app.UseDeveloperExceptionPage();
 			}
+			app.UseDefaultFiles();
+			app.UseStaticFiles();
 
 			app.Run(async (context) =>
 			{
-				await context.Response.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+				await context.Response.WriteAsync(_config["MyKey"]);
 			});
 		}
 	}
